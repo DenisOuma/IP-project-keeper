97ca47a36d137656b8bdd6438bb82bb81f7b977e
DenisOuma <dennisouma38@gmail.com>
2022-10-05 18:52:16 +0300

    User able to login and get token

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -44,23 +47,23 @@ router.post(
 			await user.save();
 			res.send("User saved");
 
-			// const payload = {
-			// 	user: {
-			// 		id: user.id,
-			// 	},
-			// };
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
 
-			// jwt.sign(
-			// 	payload,
-			// 	config.get("jwtSecret"),
-			// 	{
-			// 		expiresIn: 360000,
-			// 	},
-			// 	(err, token) => {
-			// 		if (err) throw err;
-			// 		res.json({ token });
-			// 	}
-			// );
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{
+					expiresIn: 660000,
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.log(err.message);
 			res.status(500).send("Server Error");
--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 
 // Definng routes
 app.use("/api/users", require("./routes/users"));
-app.use("/api/ath", require("./routes/auth"));
+app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
