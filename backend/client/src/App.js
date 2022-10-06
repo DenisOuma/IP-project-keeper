9d30ab074cfe84a3e81a623b1d62bc858b033cf6
DenisOuma <dennisouma38@gmail.com>
2022-10-06 22:52:48 +0300

    Adding Auth Context and initializing the state

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -5,22 +5,25 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import AboutPage from "./components/pages/AboutPage";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/authentication/AuthState";
 
 function App() {
 	return (
-		<ContactState>
-			<Router>
-				<>
-					<Navbar />
-					<div className="container">
-						<Routes>
-							<Route exact path="/" element={<HomePage />} />
-							<Route exact path="/about" element={<AboutPage />} />
-						</Routes>
-					</div>
-				</>
-			</Router>
-		</ContactState>
+		<AuthState>
+			<ContactState>
+				<Router>
+					<>
+						<Navbar />
+						<div className="container">
+							<Routes>
+								<Route exact path="/" element={<HomePage />} />
+								<Route exact path="/about" element={<AboutPage />} />
+							</Routes>
+						</div>
+					</>
+				</Router>
+			</ContactState>
+		</AuthState>
 	);
 }
 
