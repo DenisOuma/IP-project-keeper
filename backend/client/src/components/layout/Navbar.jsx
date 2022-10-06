a5f2bb4d76debab8ab3fabfbf55e98d32a01ac77
DenisOuma <dennisouma38@gmail.com>
2022-10-06 08:31:31 +0300

    Setting up the material ui icons | adding packages json libraries for the frontend or client side

---
+++ backend/client/src/components/layout/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LockPersonTwoToneIcon from "@mui/icons-material/LockPersonTwoTone";
+
+function Navbar() {
+	return (
+		<div className="navbar bg-primary">
+			<h2>
+				<LockPersonTwoToneIcon fontSize="large" />
+				Keeper
+			</h2>
+		</div>
+	);
+}
+
+export default Navbar;
