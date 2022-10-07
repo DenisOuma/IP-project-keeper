0a119adeb34ab68068d0d3d69600d275ae953535
DenisOuma <dennisouma38@gmail.com>
2022-10-07 21:46:23 +0300

    changes made | starting deployment

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,16 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
+// Production
+// if(process.env.NODE_ENV === 'production') {
+// 	// Set static folder
+// 	app.use(express.static('client/build'));
+
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
